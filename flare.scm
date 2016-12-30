ccb718c5d9d457b8294085040464646f1a965e4d
Luiz de Milon <luiz.de.milon@gmail.com>
2016-12-30 01:31:16 -0300

    flare.scm: Simplify function a bit, edit style, document.

--- flare.scm
+++ flare.scm
@@ -1,19 +1,20 @@
-(define default-bars '(#\▁ #\▂ #\▃ #\▄ #\▅ #\▆ #\▇ #\█))
+(define default-bars '(▁ ▂ ▃ ▄ ▅ ▆ ▇ █))
 
-; create spark line from list using the bars defined by b
-(define spark-line
+; generate spark line from list using the bars defined by b
+(define generate-line
   (lambda (l b)
-    (list->symbol
-      (map (lambda (index)
-             (list-ref b index))
-           (map
-             (lambda (number)
-               (floor-quotient
-                 (* (1- (length b)) number)
-                 (apply max l)))
-             l)))))
+    ; get index and return the corresponding bar
+    (map (lambda (index)
+           (list-ref b index))
+         ; see Formula section of README
+         (map (lambda (number)
+                (floor-quotient
+                  (* (1- (length b))
+                     number)
+                  (apply max l)))
+               l))))
 
 ; spark with default bars
 (define spark
   (lambda (l)
-    (spark-line l default-bars)))
+    (generate-line l default-bars)))
