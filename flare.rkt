2b2a9b3b8589645652ed1b5bdb6c4eaae14258fd
Luiz de Milon <luiz.de.milon@gmail.com>
2017-12-19 21:26:47 -0300

    use compose instead of mapping twice

--- flare.rkt
+++ flare.rkt
@@ -4,20 +4,20 @@
   spark
   generate-line)
 
-(define default-bars '(▁ ▂ ▃ ▄ ▅ ▆ ▇ █))
+; spark with default bars
+(define (spark l)
+  (let ((default-bars '(▁ ▂ ▃ ▄ ▅ ▆ ▇ █)))
+    (generate-line l default-bars)))
 
 ; generate spark line from list using the bars defined by b
 (define (generate-line l b)
   ; get index and return the corresponding bar
-  (map (curry list-ref b)
-       ; see Formula section of README
-       (map (lambda (number)
-              (floor (quotient
-                       (* (sub1 (length b))
-                          number)
-                       (apply max l))))
-            l)))
-
-; spark with default bars
-(define (spark l)
-  (generate-line l default-bars))
+  (map
+    ; see Formula section of README
+    (compose (curry list-ref b)
+             (lambda (number)
+               (floor (quotient
+                        (* (sub1 (length b))
+                           number)
+                        (apply max l)))))
+         l))
